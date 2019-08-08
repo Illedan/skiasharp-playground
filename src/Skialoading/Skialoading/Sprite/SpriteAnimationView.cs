b201a883ed2141e056e217024e1046e584e34e9b
Erik Kvanli <ekv@dips.no>
2019-08-08 22:26:18 +0200

    Added flying bird animation

--- src/Skialoading/Skialoading/Sprite/SpriteAnimationView.cs
+++ src/Skialoading/Skialoading/Sprite/SpriteAnimationView.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SkiaLoading.Extensions;
+using SkiaLoading.Time;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
@@ -9,6 +11,10 @@ namespace SkiaLoading.Sprite
 {
     public class SpriteAnimationView : SKCanvasView
     {
+        private int index;
+        private List<SKBitmap> bitmaps = new List<SKBitmap>();
+        private double sumTime;
+        private TimeTracker timeTracker = new TimeTracker();
         public SpriteAnimationView()
         {
             PaintSurface += Redraw;
@@ -20,9 +26,25 @@ namespace SkiaLoading.Sprite
         }
 
         public List<string> Sprites { get; set; } = new List<string>();
+        public double SwapTime { get; set; } = 0.5;
 
         private void Redraw(object sender, SKPaintSurfaceEventArgs e)
         {
+            if(bitmaps.Count != Sprites.Count)
+            {
+                bitmaps.Clear();
+                bitmaps.AddRange(Sprites.Select(s => BitmapExtensions.LoadBitmapResource(s)));
+            }
+
+            var time = timeTracker.GetTime();
+            sumTime += time;
+            if(sumTime > SwapTime)
+            {
+                index++;
+                if (index >= bitmaps.Count) index = 0;
+                sumTime = 0;
+            }
+
             var canvas = e.Surface.Canvas;
             var width = e.Info.Width;
             var height = e.Info.Height;
@@ -30,8 +52,7 @@ namespace SkiaLoading.Sprite
             canvas.Save();
             canvas.Translate(new SKPoint(width / 2.0f, height / 2.0f));
 
-            var bitmap = BitmapExtensions.LoadBitmapResource("Bird1.png");
-            canvas.DrawBitmap(bitmap, new SKPoint(0, 0));
+            canvas.DrawBitmap(bitmaps[index], new SKPoint(0, 0));
 
             canvas.Restore();
         }
