25b5810640e4d1aa8aaf9be0918355ff76d66a55
Erik Kvanli <0erikkvanli0@gmail.com>
2020-01-28 16:34:17 +0100

    Added reference and lines

--- src/Skialoading/Skialoading/Graph/GraphLayout.cs
+++ src/Skialoading/Skialoading/Graph/GraphLayout.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using DIPS.Xamarin.UI.Controls.Slidable;
 using SkiaSharp;
@@ -9,13 +10,24 @@ namespace SkiaLoading.Graph
 {
     public class GraphLayout : SlidableLayout
     {
-        private static readonly SKPaint GraphColor = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.DarkCyan, StrokeWidth=5 };
+        private static readonly SKPaint GraphColor = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.MediumPurple, StrokeWidth=5 };
+        private static readonly SKPaint VerticalLineColor = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.DimGray, StrokeWidth = 2 };
+
+        private static readonly SKPaint ReferenceColor = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.DarkCyan,
+            StrokeWidth = 5,
+            StrokeCap = SKStrokeCap.Square,
+            PathEffect = SKPathEffect.CreateDash(new []{10f, 20f}, 25)
+        };
+
         private double m_lastIndex, m_offset, m_selectedIndex;
         private readonly object m_lock = new object();
         private readonly SKCanvasView m_canvas = new SKCanvasView
         {
             VerticalOptions = LayoutOptions.FillAndExpand,
-            HorizontalOptions = LayoutOptions.FillAndExpand
+            HorizontalOptions = LayoutOptions.FillAndExpand,
         };
 
         public GraphLayout()
@@ -34,10 +46,18 @@ namespace SkiaLoading.Graph
             if (width < 1 || Repository == null) return;
             lock (m_lock)
             {
+                
+                var points = new List<SKPoint>();
+
                 canvas.Clear();
                 var max = double.MinValue;
                 var min = double.MaxValue;
-                var itemWidth = GetItemWidth();
+                if(Reference != null)
+                {
+                    max = Reference.Max;
+                    min = Reference.Min;
+                }
+                var itemWidth = width*ElementWidth;
                 var totalWidth = (float)(width / itemWidth);
                 for (var i = m_selectedIndex - totalWidth ; i <= m_selectedIndex + totalWidth; i++)
                 {
@@ -49,20 +69,39 @@ namespace SkiaLoading.Graph
                     if (point.DValue > max) max = point.DValue.Value;
                     if (point.DValue < min) min = point.DValue.Value;
                 }
+
                 var diff = max - min;
                 max += diff * 0.2;
                 min -= diff * 0.2;
+                if (Reference != null)
+                {
+                    var x0 = (float)(-width*2 + itemWidth * (m_selectedIndex - m_lastIndex));
+                    var x1 = (float)(width * 1.5);
+                    var y0 = GetValue(Reference.Max, max, min, height);
+                    var y1 = GetValue(Reference.Min, max, min, height);
+                    canvas.DrawLine(new SKPoint(x0, y0), new SKPoint(x1, y0), ReferenceColor);
+                    canvas.DrawLine(new SKPoint(x0, y1), new SKPoint(x1, y1), ReferenceColor);
+                }
                 SKPoint? prev = null;
+                float? firstX = null;
+                float lastX = 0;
                 for (var i = m_selectedIndex - totalWidth; i <= m_selectedIndex + totalWidth; i++)
                 {
                     var pos = (int)Math.Floor(i);
                     var hasValue = Repository.TryGetPoint(Math.Abs(pos), out var point);
                     if (pos < Config.MinValue || pos > Config.MaxValue) continue;
-                    if (!hasValue || point.DValue == null) continue;
+                    var x = (float)(width/2 + itemWidth * (i - m_lastIndex+0.5));
+                    canvas.DrawLine(x, height, x, 0, VerticalLineColor);
 
-                    var x = (float)(width/2 + itemWidth * (i - m_lastIndex));
+                    if (!hasValue || point.DValue == null) continue;
+                    lastX = x;
+                    if(firstX == null)
+                    {
+                        firstX = x;
+                    }
+
                     var y = GetValue(point.DValue.Value, max, min, height);
-                    canvas.DrawCircle(x, y, 10, GraphColor);
+                    canvas.DrawCircle(x, y, 15, GraphColor);
                     var currentPos = new SKPoint(x, y);
                     if (prev != null)
                     {
@@ -70,6 +109,23 @@ namespace SkiaLoading.Graph
                     }
 
                     prev = currentPos;
+                    points.Add(currentPos);
+                }
+                
+                if(firstX != null)
+                {
+                    var color = GraphColor.Color.WithAlpha((byte)20);
+
+                    var fillPaint = new SKPaint
+                    {
+                        Style = SKPaintStyle.Fill,
+                        Color = color
+                    };
+
+                    points.Add(new SKPoint(lastX, height));
+                    points.Add(new SKPoint(firstX.Value, height));
+
+                    canvas.DrawPoints(SKPointMode.Polygon, points.ToArray(), fillPaint);
                 }
             }
         }
@@ -118,6 +174,19 @@ namespace SkiaLoading.Graph
         {
             get => (IDataRepository)GetValue(RepositoryProperty);
             set => SetValue(RepositoryProperty, value);
+        }
+
+        public static readonly BindableProperty ReferenceProperty = BindableProperty.Create(
+            nameof(Reference),
+            typeof(ReferenceArea),
+            typeof(GraphLayout),
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (s, e, n) => ((GraphLayout)s).Redraw());
+
+        public ReferenceArea Reference
+        {
+            get => (ReferenceArea)GetValue(ReferenceProperty);
+            set => SetValue(ReferenceProperty, value);
         }
     }
 }
--- src/Skialoading/Skialoading/Graph/GraphViewModel.cs
+++ src/Skialoading/Skialoading/Graph/GraphViewModel.cs
@@ -5,28 +5,44 @@ using DIPS.Xamarin.UI.Extensions;
 
 namespace SkiaLoading.Graph
 {
-    public class GraphViewModel : DataRepository, INotifyPropertyChanged
+    public class GraphViewModel : INotifyPropertyChanged
     {
         private SlidableProperties slidableProperties;
 
         public GraphViewModel()
         {
-            AddPoint(new GraphPoint(0, "T1", 5));
-            AddPoint(new GraphPoint(1, "T2", 2));
-            AddPoint(new GraphPoint(2, "T3", 1));
-            AddPoint(new GraphPoint(5, "T4", 7));
-            AddPoint(new GraphPoint(6, "T5", 3));
-            AddPoint(new GraphPoint(8, "T6", 4));
-            AddPoint(new GraphPoint(10, "T7", 2));
-            var rnd = new Random();
-            for(var i = 11; i < 100; i++)
-            {
-                if (rnd.NextDouble() < 0.3) continue;
-                AddPoint(new GraphPoint(i, "T"+i, rnd.Next(0, 20)));
-            }
             SlidableProperties = new SlidableProperties(-20);
+            //AddPoint(new GraphPoint(0, "T1", 5));
+            //AddPoint(new GraphPoint(1, "T2", 2));
+            //AddPoint(new GraphPoint(2, "T3", 1));
+            //AddPoint(new GraphPoint(5, "T4", 7));
+            //AddPoint(new GraphPoint(6, "T5", 3));
+            //AddPoint(new GraphPoint(8, "T6", 4));
+            //AddPoint(new GraphPoint(10, "T7", 2));
+            var rnd = new Random();
+            for(var i = 0; i < 100; i++)
+            {
+                if (rnd.NextDouble() < 0.2) continue;
+                if (rnd.NextDouble() < 0.7)
+                    Graph1.AddPoint(new GraphPoint(i, "T" + i, rnd.Next(3, 7)));
+                else
+                    Graph1.AddPoint(new GraphPoint(i, "T"+i, rnd.Next(0, 10)));
+            }
+
+            for (var i = 0; i < 100; i++)
+            {
+                if (rnd.NextDouble() < 0.1) continue;
+                if (rnd.NextDouble() < 0.3)
+                    Graph2.AddPoint(new GraphPoint(i, "T" + i, rnd.Next(3, 7)));
+                else
+                    Graph2.AddPoint(new GraphPoint(i, "T" + i, rnd.Next(0, 14)));
+            }
         }
 
+        public DataRepository Graph1 { get; } = new DataRepository();
+        public DataRepository Graph2 { get; } = new DataRepository();
+
+        public ReferenceArea Reference { get; } = new ReferenceArea(3, 7);
         public SliderConfig Config => new SliderConfig(-100, 0);
 
         public SlidableProperties SlidableProperties { get => slidableProperties; set => PropertyChanged?.RaiseWhenSet(ref slidableProperties, value); }
