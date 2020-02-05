c7a610ab13be4292159dd664fbedbe91a5c0b695
Erik Kvanli <0erikkvanli0@gmail.com>
2020-02-05 11:09:18 +0100

    More work on graph and repro

--- src/Skialoading/Skialoading/Graph/GraphLayout.cs
+++ src/Skialoading/Skialoading/Graph/GraphLayout.cs
@@ -10,7 +10,7 @@ namespace SkiaLoading.Graph
 {
     public class GraphLayout : SlidableLayout
     {
-        private static readonly SKPaint GraphColor = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.MediumPurple, StrokeWidth=7 };
+        private static readonly SKPaint GraphColor = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.MediumPurple, StrokeWidth = 7 };
         private static readonly SKPaint VerticalLineColor = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColors.LightGray, StrokeWidth = 2 };
         private static readonly SKPaint FillPaint = new SKPaint
         {
@@ -26,11 +26,16 @@ namespace SkiaLoading.Graph
             Color = SKColors.DarkCyan,
             StrokeWidth = 2,
             StrokeCap = SKStrokeCap.Square,
-            PathEffect = SKPathEffect.CreateDash(new []{10f, 20f}, 25)
-        };
+            PathEffect = SKPathEffect.CreateDash(new[] { 10f, 20f }, 25)
+        }; 
 
         private double m_lastIndex, m_offset, m_selectedIndex;
         private readonly object m_lock = new object();
+        private readonly SKCanvasView m_tooltipCanvas = new SKCanvasView
+        {
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+        };
         private readonly SKCanvasView m_canvas = new SKCanvasView
         {
             VerticalOptions = LayoutOptions.FillAndExpand,
@@ -42,7 +47,10 @@ namespace SkiaLoading.Graph
             VerticalOptions = LayoutOptions.FillAndExpand;
             HorizontalOptions = LayoutOptions.FillAndExpand;
             m_canvas.PaintSurface += Redraw;
-            Content = m_canvas;
+            var grid = new Grid();
+            grid.Children.Add(m_canvas);
+            grid.Children.Add(m_tooltipCanvas);
+            Content = grid;
         }
 
         private void Redraw(object sender, SKPaintSurfaceEventArgs e)
@@ -58,14 +66,14 @@ namespace SkiaLoading.Graph
                 canvas.Clear();
                 var max = double.MinValue;
                 var min = double.MaxValue;
-                if(Reference != null)
+                if (Reference != null)
                 {
                     max = Reference.Max;
                     min = Reference.Min;
                 }
-                var itemWidth = width*ElementWidth;
+                var itemWidth = width * ElementWidth;
                 var totalWidth = (float)(width / itemWidth);
-                for (var i = m_selectedIndex - totalWidth ; i <= m_selectedIndex + totalWidth; i++)
+                for (var i = m_selectedIndex - totalWidth; i <= m_selectedIndex + totalWidth; i++)
                 {
                     var pos = (int)Math.Floor(i);
                     var hasValue = Repository.TryGetPoint(Math.Abs(pos), out var point);
@@ -81,13 +89,14 @@ namespace SkiaLoading.Graph
                 min -= diff * 0.2;
                 if (Reference != null)
                 {
-                    var x0 = (float)(-width*2 + itemWidth * (m_selectedIndex - m_lastIndex));
+                    var x0 = (float)(-width * 2 + itemWidth * (m_selectedIndex - m_lastIndex));
                     var x1 = (float)(width * 1.5);
                     var y0 = GetValue(Reference.Max, max, min, height);
                     var y1 = GetValue(Reference.Min, max, min, height);
                     canvas.DrawLine(new SKPoint(x0, y0), new SKPoint(x1, y0), ReferenceColor);
                     canvas.DrawLine(new SKPoint(x0, y1), new SKPoint(x1, y1), ReferenceColor);
                 }
+
                 SKPoint? prev = null;
                 float? firstX = null;
                 float lastX = 0;
@@ -99,7 +108,7 @@ namespace SkiaLoading.Graph
                         var pos = (int)Math.Floor(i);
                         if (pos < Config.MinValue || pos > Config.MaxValue) continue;
                         var hasValue = Repository.TryGetPoint(Math.Abs(pos), out var point);
-                        var x = (float)(width/2 + itemWidth * (i - m_lastIndex+0.5));
+                        var x = (float)(width / 2 + itemWidth * (i - m_lastIndex + 0.5));
                         canvas.DrawLine(x, height, x, 0, VerticalLineColor);
 
                         if (!hasValue || point.DValue == null) continue;
@@ -120,11 +129,11 @@ namespace SkiaLoading.Graph
 
                         prev = currentPos;
                         points.Add(currentPos);
-                        
+
                         path.LineTo(currentPos);
                     }
-                
-                    if(firstX != null)
+
+                    if (firstX != null)
                     {
                         path.LineTo(new SKPoint(lastX, height));
                         path.LineTo(new SKPoint(firstX.Value, height));
@@ -159,7 +168,7 @@ namespace SkiaLoading.Graph
                 m_selectedIndex = selectedIndex;
             }
 
-            if(Width < 0.1)
+            if (Width < 0.1)
             {
                 return;
             }
@@ -189,7 +198,7 @@ namespace SkiaLoading.Graph
             propertyChanged: (s, e, n) => ((GraphLayout)s).Redraw());
 
         public ReferenceArea Reference
-        {
+        { 
             get => (ReferenceArea)GetValue(ReferenceProperty);
             set => SetValue(ReferenceProperty, value);
         }
---
+++ src/Skialoading/Skialoading/Graph/GraphPin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SkiaSharp.Views.Forms;
+
+namespace SkiaLoading.Graph
+{
+    public class GraphPin 
+    {
+        private readonly SKCanvasView m_canvas;
+
+        public GraphPin(SKCanvasView canvas)
+        {
+            this.m_canvas = canvas;
+            m_canvas.PaintSurface += Redraw;
+        }
+
+        private void Redraw(object sender, SKPaintSurfaceEventArgs e)
+        {
+
+        }
+
+        public void Update()
+        {
+            m_canvas.InvalidateSurface();
+        }
+    }
+}
